7e5c7eba8aa7e314574b5ed1f1f78db887a787e6
Jecelyn Yeen <jec@google.com>
2023-03-27 09:30:05 +0200

    Update to Angular 15

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { AsyncStackTaggingZoneSpec } from './zone-async-stack-tagging';
-
 if (environment.production) {
   enableProdMode();
 }
 
-Zone.current.fork(new AsyncStackTaggingZoneSpec()).run(() => {
-  platformBrowserDynamic()
+platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .catch((err) => console.error(err));
-});
